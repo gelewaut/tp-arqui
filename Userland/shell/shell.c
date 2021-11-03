f505c401a16921e7be90bc6b58ce8f88aea91f06
bdelasoie <bdelasoie@itba.edu.ar>
2021-11-03 19:51:28 -0300

    userlib functions and shell

--- Userland/shell/shell.c
+++ Userland/shell/shell.c
@@ -1,34 +1,194 @@
 #include <shell.h>
+#include <userlibc.h>
 #include <naiveConsole.h>
-#include <commands.h>
 
-#define PROMPT "$>\t"
-#define MAX_BUFFER 1024
+#include <fortune.h>
+#include <shell.h>
 
-static char * buffer[MAX_BUFFER];
+#define PROMPT "$>"
+#define MAX_BUFFER 128
+#define MAX_COMMAND_LENGHT 5
+#define MAX_ARGS 3
+#define MAX_ARG_LENGHT 5
+#define NUMBER_OF_COMMANDS 2
+
+#define BACKSPACE 127
+#define ENTER '\n'
+
+#define EXIT_SUCCESS 1
+#define EXIT_FAILURE 0
+
+static char * shell_buffer[MAX_BUFFER] = {0};
+static char * command_buffer[MAX_COMMAND_LENGHT] = {0};
+static char ** args[MAX_ARGS][MAX_ARG_LENGHT];
 static int bufferIdx = 0;
-static char token;
+
+static char * valid_commands[NUMBER_OF_COMMANDS] = {
+    "help", // 0
+    "fortune" // 1
+    "time" // 2
+    "inforeg" // 3
+    "printmem" // 4
+};
+
+static uint8_t args_for_command[NUMBER_OF_COMMANDS] = {
+    0,
+    0,
+    0,
+    0,
+    0
+};
+ 
 
 void init_shell() {
-    while(1) {
-        while( (token = getChar()) != '\n') {
-            buffer[bufferIdx++] = token;
-            putChar(token);
+    // setup
+    ncClear();
+    shell_welcome();
+
+    //loop
+    shell_loop();
+
+    //shutdown
+
+}
+
+void shell_welcome() {
+    ncPrintln("WELCOME TO SHELL");
+}
+
+void shell_loop() {
+    uint8_t mustContinue = 1;
+
+    do {
+        ncPrint(PROMPT);
+        shell_read_line();
+        shell_parse_line();
+        mustContinue = shell_execute();
+        clear_buffer();
+
+    } while (mustContinue);
+}
+
+void shell_read_line() {
+    uint8_t c;
+
+    while ( (c = getChar()) != ENTER) {
+        if (c == BACKSPACE) {
+            shell_buffer[bufferIdx--] = 0;
+        } else {
+            shell_buffer[bufferIdx++] = c;
         }
-        interpret_line(buffer);
+            putChar(c);
     }
 }
 
-void interpret_line(const char * buffer) {
-    if (isCommand(buffer)) {
-        runCommand(buffer);
-    } else {
-        ncPrintln("Comando no conocido. Consulte los comandos con 'help'");
+void shell_parse_line() {
+    uint8_t args_counter = 0;
+    uint8_t buffer_idx = 0;
+    uint8_t aux_idx = 0;
+
+    uint8_t token;
+
+    while(shell_buffer[buffer_idx] && args_counter <= MAX_ARGS) {
+        token = shell_buffer[buffer_idx++];
+        if (token == ' ') {
+            args_counter++;
+            aux_idx = 0;
+            continue;
+        }
+        if (args_counter == 0) {
+            command_buffer[aux_idx++] = token;
+        } else {
+            args[args_counter-1][aux_idx++] = token;
+        }
     }
+}
+
+uint8_t shell_execute() {
+    uint8_t cmd, result;
+    if ( (cmd = isCommand(command_buffer)) != -1) {
+        result = runCommand(cmd);
+    } else {
+        ncPrintln("Unknown command.");
+    }
+
+    cleanup();
+    ncNewline();
+    return result;
+}
+
+int8_t isCommand(const char * command) {
+    for (int i = 0; i < NUMBER_OF_COMMANDS; i++) {
+        if (string_compare(command, valid_commands[i])) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+uint8_t runCommand(uint8_t cmd) {
+    switch (cmd) {
+        // En cada caso puedo definir si le paso args si es necesario
+        // y cuantos les paso
+        case 0: {
+            return helpCommand();
+        }
+        case 1: {
+            return fortuneCommand();
+        }
+        case 2: {
+            return timeCommand();
+        }
+        case 3: {
+            return inforegCommand();
+        }
+        case 4: {
+            return printmemCommand();
+        }
+    }
+}
+
+uint8_t helpCommand() {
+    ncPrintln("These are the available commands");
+    for (int i = 0; i < NUMBER_OF_COMMANDS; i++) {
+        ncPrintln(valid_commands[i]);
+    }
+    return EXIT_SUCCESS;
+}
+
+uint8_t timeCommand() {
+    uint8_t hours, minutes, seconds;
+
+    hours = getHours();
+    minutes = getMinutes();
+    seconds = getSeconds();
+
+    printf("TIME: %d:%d:%d\n", hours, minutes, seconds);
+
+    return EXIT_SUCCESS;
+}
+
+
+
+void cleanup() {
     clear_buffer();
+    clear_command_buffer();
+    clear_args();
 }
 
 void clear_buffer() {
-    for(int i = 0; i <= bufferIdx; i++) buffer[i] = 0;
+    for(int i = 0; i <= bufferIdx; i++) shell_buffer[i] = 0;
     bufferIdx = 0;
+}
+
+void clear_command_buffer() {
+    for (int i = 0; i < MAX_COMMAND_LENGHT; i++) command_buffer[i] = 0;
+}
+
+void clear_args() {
+    for (int i = 0; i < MAX_ARGS; i++) {
+        for (int j = 0; j < MAX_ARG_LENGHT; j++) {
+            args[i][j] = 0;
+        }
+    }
 }
--- Userland/userlibc.c
+++ Userland/userlibc.c
@@ -1,6 +1,50 @@
 #include <userlibc.h>
 #include <stdarg.h>
-#include <stdio.h>
+#include <stdint.h>
+#include <syscalls.h>
+
+#define BUFFER_SIZE 32
+
+#define STDIN 0
+#define STDOUT 1
+#define STDERR 2
+
+#define CLOCK_HOURS 7
+#define CLOCK_MINUTES 4 
+#define CLOCK_SECONDS 2
+
+static char * buffer[BUFFER_SIZE] = {0};
+
+static char hexArray[16]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+
+char * numToStr(unsigned long int num, int base){
+	cleanBuffer();
+    char aux = num%base;
+    int i = BUFFER_SIZE;
+    buffer[i]=0;
+	if(num==0)
+		buffer[i--]='0';
+    while(i>=0 && num>0){
+        aux = num%base;
+        buffer[i]=hexArray[aux];
+        i--;
+        num/=base;
+    }
+
+    return buffer+i+1;
+}
+
+int strToNum(char* str){
+    int num=0;
+    
+    for(int i=0; str[i] ; i++){
+        num*=10;
+        if(str[i]<'0' || str[i]>'9' )//no es un numero valido
+            return -1;
+        num+=str[i]-'0';
+    }
+    return num;
+}
 
 int string_compare(const char * s1, const char * s2) {
     int i;
@@ -10,3 +54,75 @@ int string_compare(const char * s1, const char * s2) {
     return s1[i] - s2[i];
 }
 
+void printf(char* string, ...){
+	int i=0, argumentCount=0;
+	while(string[i]){
+		if(string[i] == '%') argumentCount++;
+		i++;
+	}
+	va_list list;
+	va_start(list, argumentCount);
+	while(*string){
+		if(*string=='\''){
+			string++;
+			sys_write(STDOUT,string, 1);
+			string++;
+		}
+		else if(*string!='%'){
+			sys_write(STDOUT,string, 1);
+			string++;
+		}
+		else{
+			string++;
+			switch (*string){
+			case 'd': printDec(va_arg(list, int));
+				break;
+			case 'c': putChar(va_arg(list, int));
+				break;
+			case 's': printf(va_arg(list, int));
+				break;
+			default:
+				break;
+			}
+			string++;
+		}
+	}
+	va_end(list);
+}
+
+uint8_t getChar() {
+    uint8_t c;
+    sys_read(STDIN, &c, 1);
+    return c;
+}
+
+void putChar(uint8_t c) {
+    sys_write(STDOUT, &c, 1);
+}
+
+void printDec(int num){
+	printf(numToStr(num,10));
+}
+
+void printHex(int num){
+	printf(numToStr(num,16));
+}
+
+void cleanBuffer(){
+	for(int i=0; i < BUFFER_SIZE; i++){
+		buffer[i]=0;
+	}
+}
+
+uint8_t getHours() {
+	sys_clock(CLOCK_HOURS);
+}
+
+uint8_t getMinutes() {
+	sys_clock(CLOCK_MINUTES);
+
+}
+
+uint8_t getSeconds() {
+	sys_clock(CLOCK_SECONDS);
+}
